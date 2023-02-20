b2189b69f05b991b65512e788db0bf8ac80b172e
Thế Huỳnh <62024025+thehuynh0124@users.noreply.github.com>
2023-02-20 21:49:39 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+from os import name
+from todolist import create_app
+
+app = create_app()
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ todolist/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask
+import os
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+load_dotenv()
+SECRET_KEY = os.environ.get("KEY")
+DB_NAME = os.environ.get("DB_NAME")
+
+def create_database(app):
+    if not os.path.exists('todolist/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print("Created DB!")
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    
+    db.init_app(app)
+    from .models import Note, User
+
+    
+    create_database(app)
+    from .user import user
+    from .views import views
+    app.register_blueprint(user)
+    app.register_blueprint(views)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "user.login"
+    login_manager.init_app(app)
+    app.permanent_session_lifetime = timedelta(minutes=5)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
